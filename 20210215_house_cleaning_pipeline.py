6531ae52dc5655ff55e6da2c803630d94dddc13d
Robert Link <rwlink3z8@gmail.com>
2021-02-15 10:51:34 -0600

    Add files via upload

---
+++ 20210215_house_cleaning_pipeline.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+import numpy as np 
+
+from data_cleaning_src import clean_lot_size, price_to_log, convert_categorical_cols, send_to_csv
+
+df = pd.read_csv('20210209mlstest.csv')
+
+df = (df.pipe(clean_lot_size)
+        .pipe(price_to_log)
+        .pipe(convert_categorical_cols)
+        .pipe(send_to_csv))
+print("data cleaned and file written")
